917f2cbd91c79d51fd7efbc5fdfd383584f9200c
Owen Donckers <odonckers@gmail.com>
2015-12-10 16:20:59 -0600

    Removed Sections
    I have removed Py2App and renamed folders.

--- Paddle Ball/Py2app/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-python setup.py py2app
-zip -r PaddleBall-Mac.zip dist/PaddleBall.app/
-mv PaddleBall-Mac.zip ../../Downloads/
-rm -rf PaddleBall.app/
-mv dist/PaddleBall.app/ .
-rm -rf build dist
